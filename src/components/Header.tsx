683cae1a7e7f1ab3cd6072efdfbc59a3cc2e86ba
Gokhan Dilek <gokhanengineering@gmail.com>
2025-01-05 11:20:32 +0800

    Header.tsx -Ignoring the logo importing typescript error for now.
    index.html
    -Added title.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+// @ts-ignore
 import logo from "../../logo.jpg"
 export default function Header() {
     
     return <div className="header">
-        <img src={logo}/>
+        <img alt="logo" src={logo}/>
         <span className="brand">Claude</span>
     </div>
 }
